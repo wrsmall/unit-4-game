b9eabc2bb792793a6f190374a24bfc430deff26e
wrsmall <william.r.small89@gmail.com>
2019-06-08 10:13:01 -0400

    got the answer number to push to the answer div working on the randomized button

---
+++ assets/jquery.js
@@ -0,0 +1,15 @@
+var answr=Math.floor(Math.random()*120)+19;
+console.log("the number is "+answr);
+$( "#target" ).append( answr );
+
+buttons=["button1","button2","button3","button4"];
+console.log(buttons);
+for(i=0;i<buttons.length;i+1){
+  var addButton=$("<button>")
+  var btnvlue=(Math.floor(Math.random())*12)+1;
+  console.log(btnvlue);
+  $('#crystal1').val(btnnum);
+
+
+}
+
